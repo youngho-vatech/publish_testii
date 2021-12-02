a139f288183a08b2d12d76aa2bfb3a8bcec61b01
김영호 <>
2021-12-02 15:35:56 +0900

    update14

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+import {
+  api as dynamoApi,
+  checkStatusToDynamoDbName,
+  createModels as dynamoCreateModels,
+  getSecretString,
+  restoreTableFromBackup
+} from "./dynamoDB/api/index.js";
+export {
+  api as mongoApi,
+  createModels as mongoCreateModels
+} from "./mongoDB/api/index.js";
+
 function test(params) {
   console.log("asdf", params);
 }
+module.exports = dynamoApi;
 module.exports = test;
