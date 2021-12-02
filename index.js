23581239f75d2e8c8f5124245be4c05ffbf96079
김영호 <>
2021-12-02 14:12:29 +0900

    update6

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import { api as mongo, createModels as mongoc } from "./mongoDB/api/mongoDB.js";
-import {
+export { api as mongo, createModels as mongoc } from "./mongoDB/api/mongoDB.js";
+export {
   api as dynamo,
   checkStatusToDynamoDbName,
   createModels as dynamoc,
   getSecretString,
   restoreTableFromBackup
 } from "./dynamoDB/api/dynamoDB.js";
-
-console.log("asdf");
