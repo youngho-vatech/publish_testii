111a6e4b734dbea81f25ad2f517d08c582abbd0a
김영호 <>
2021-12-06 11:17:15 +0900

    update 38

--- utils/etc.js
+++ utils/etc.js
@@ -1,4 +1,4 @@
-import moment from "moment";
+const moment = require("moment");
 
 const getType = object => {
   if (object.constructor === Object) return "Map";
@@ -144,7 +144,7 @@ const splitForEach = (arr, n, tableName) => {
   return res;
 };
 
-export {
+module.exports = {
   getType,
   getRequiredKeys,
   getHashRangeKeyIndex,
